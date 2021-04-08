34c21c3a32ffeec4c728d5bbd2c397cd169d03b6
emil <emilharoutunian@gmail.com>
2021-04-08 13:04:19 -0700

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Emil Haroutunian on 4/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
